e07fe6b8439a47ab8c6e7b7be3aea2f2b42bcd63
panjiangfeng <1415161920@qq.com>
2023-10-16 00:51:37 +0800

    初始化项目

---
+++ guli_mall_coupon/src/test/java/com/test/guli_mall_coupon/GuliMallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.guli_mall_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ guli_mall_member/src/test/java/com/test/guli_mall_member/GuliMallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.guli_mall_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
