31fbe6d2c696a683ba3adde71eed73710d90bd98
panjiangfeng <1415161920@qq.com>
2023-10-20 00:29:48 +0800

    feat:构建通用工程

--- guli_mall_coupon/src/main/java/com/test/gulimall/coupon/GuliMallCouponApplication.java
+++ guli_mall_coupon/src/main/java/com/test/gulimall/coupon/GuliMallCouponApplication.java
@@ -2,9 +2,11 @@ package com.test.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class GuliMallCouponApplication {
+public class  GuliMallCouponApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GuliMallCouponApplication.class, args);
