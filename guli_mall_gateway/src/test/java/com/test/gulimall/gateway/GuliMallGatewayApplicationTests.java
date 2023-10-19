657cc3b46b24b4f8a21eb8f505734dec5b4a8ca4
panjiangfeng <1415161920@qq.com>
2023-10-20 00:29:39 +0800

    feat:构建通用工程

---
+++ guli_mall_gateway/src/test/java/com/test/gulimall/gateway/GuliMallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gulimall.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
