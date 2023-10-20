6df469dcd79e2108c372a6a201e27c51714989f7
panjiangfeng <1415161920@qq.com>
2023-10-20 23:23:10 +0800

    feat:构建通用工程

---
+++ guli_mall_gateway/src/main/java/com/test/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.test.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * Description:
+ * User: 剑指屎山
+ * Date: 2023-10-20-23:05
+ */
+@Configuration
+public class GulimallCorsConfiguration {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
